841527104170d57fadcb1308a11fe82c7f44ac09
Lucas <lucas.silva.me2@gmail.com>
2024-04-28 14:50:08 -0300

    correções de falhas no codigo
    Exercício sobre introdução a POO pelo DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iph = new Iphone("218-947", "g77", "1111111", 128);
+iph.Ligar();
+iph.ReceberLigacao();
+iph.InstalarAplicativo("whatssap");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nok = new Nokia("178-023", "indestrutivel", "222222", 4);
+nok.Ligar();
+nok.ReceberLigacao();
+nok.InstalarAplicativo("radio");
